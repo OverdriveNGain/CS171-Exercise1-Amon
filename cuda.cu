a9572df1d4dca9dfea35e8a51716fb2908f43fed
Jeremy Amon <jdamon@up.edu.ph>
2022-10-04 21:41:05 +0800

    Finished e1n2 answer

--- cuda.cu
+++ cuda.cu
@@ -1,57 +1,85 @@
 #include <stdio.h>
 #include <cuda.h>
 
-// Kernel
-__global__ void increment_vector(float *matrix1, int dimensionLength)
-{
+__global__
+void kernel_1t1e(float *matrixOut, float *matrix1, float *matrix2, int length1D, int matrixDimLen){
     int i = threadIdx.x + blockDim.x * blockIdx.x;
 
-    if (i < dimensionLength*dimensionLength) {
-        matrix1[i] += 1;
-        // printf("%f\n", matrix1[i]);
+    if (i < length1D) matrixOut[i]  = matrix1[i] + matrix2[i];
+}
+
+__global__
+void kernel_1t1r(float *matrixOut, float *matrix1, float *matrix2, int length1D, int matrixDimLen){
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+    for (int j = 0; j < matrixDimLen; j++)
+        if (i + j < length1D) matrixOut[i + j]  = matrix1[i + j] + matrix2[i + j];
+}
+
+__global__
+void kernel_1t1c(float *matrixOut, float *matrix1, float *matrix2, int length1D, int matrixDimLen){
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+
+    for (int j = 0; j < matrixDimLen; j++){
+        int index = i + j * matrixDimLen;
+        if (index < length1D) matrixOut[index]  = matrix1[index] + matrix2[index];
     }
 }
 
-__global__
-void kernel_1t1e(){
-    
-}
-
-__global__
-void kernel_1t1r(){
-    
-}
-
-__global__
-void kernel_1t1c(){
-    
-}
-
 __host__
-void matrixAdd(float** output, float** matrix1, float** matrix2, int dimensionLength){
-    size_t arrayByteSize = dimensionLength*dimensionLength*sizeof(float);
+void matrixAdd(float*** output, float*** matrix1, float*** matrix2, int dimensionLength){
+    int flattenedLength = dimensionLength*dimensionLength;
+    size_t arrayByteSize = flattenedLength*sizeof(float);
 
-    // Allocate memory for arrays d_A, d_B, and d_C on device
+    // Flatten input arrays
+    float* matrix1Flat = (float*) malloc(arrayByteSize);
+    float* matrix2Flat = (float*) malloc(arrayByteSize);
+    float* outputFlat = (float*) malloc(arrayByteSize);
+
+    // Copy data to flattened input arrays
+    for (int i = 0; i < dimensionLength; i++){
+        for (int j = 0; j < dimensionLength; j++){
+            matrix1Flat[i * dimensionLength + j] = (*matrix1)[i][j];
+            matrix2Flat[i * dimensionLength + j] = (*matrix2)[i][j];
+            outputFlat[i * dimensionLength + j] = (*output)[i][j];
+        }
+    }
+
+    // Allocate memory for arrays on device
     float *matrix1_d, *matrix2_d, *matrixOutput_d;
     cudaMalloc(&matrix1_d, arrayByteSize);
     cudaMalloc(&matrix2_d, arrayByteSize);
     cudaMalloc(&matrixOutput_d, arrayByteSize);
 
-    // Copy data from host arrays A and B to device arrays d_A and d_B
-    // cudaMemcpy(matrix1_d, *matrix1, floatArrSize, cudaMemcpyHostToDevice);
-    cudaMemcpy(matrix1_d, *matrix1, arrayByteSize, cudaMemcpyHostToDevice);
-    // cudaMemcpy(matrix2_d, *matrix2, floatArrSize, cudaMemcpyHostToDevice);
+    // Copy to device
+    cudaMemcpy(matrix1_d, matrix1Flat, arrayByteSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(matrix2_d, matrix2Flat, arrayByteSize, cudaMemcpyHostToDevice);
 
     // Launch kernel
-    increment_vector<<< 1, dimensionLength * dimensionLength >>>(matrix1_d, dimensionLength);
+    // make more reasonable
+    // kernel_1t1e<<< 1, flattenedLength >>>(matrixOutput_d, matrix1_d, matrix2_d, flattenedLength, dimensionLength);
+    // kernel_1t1r<<< 1, flattenedLength >>>(matrixOutput_d, matrix1_d, matrix2_d, flattenedLength, dimensionLength);
+    kernel_1t1c<<< 1, flattenedLength >>>(matrixOutput_d, matrix1_d, matrix2_d, flattenedLength, dimensionLength);
 
-    // Copy data from device array d_C to host array C
-    cudaMemcpy(*matrix1, matrix1_d, arrayByteSize, cudaMemcpyDeviceToHost);
+    // Copy data from device output array to flattened host array
+    cudaMemcpy(outputFlat, matrixOutput_d, arrayByteSize, cudaMemcpyDeviceToHost);
+
+    // Copy data from flattened output array to output array
+    for (int i = 0; i < dimensionLength; i++){
+        for (int j = 0; j < dimensionLength; j++){
+            (*output)[i][j] = outputFlat[i * dimensionLength + j];
+        }
+    }
 
     // Free GPU memory
     cudaFree(matrix1_d);
     cudaFree(matrix2_d);
     cudaFree(matrixOutput_d);
+
+    // Free flattened input arrays
+    free(matrix1Flat);
+    free(matrix2Flat);
+    free(outputFlat);
 }
 
 __host__
@@ -60,7 +88,7 @@ void matrixAlloc(float** matrix, int dimensionLength){
 }
 
 __host__
-void matrixPrint(float** matrix, int dimensionLength){
+void matrixPrint(float*** matrix, int dimensionLength){
     for (int i = 0; i < dimensionLength; i++){
         if (i == 0)
             printf("{{ ");
@@ -69,9 +97,9 @@ void matrixPrint(float** matrix, int dimensionLength){
         
         for (int j = 0; j < dimensionLength; j++){
             if (j == dimensionLength - 1)
-                printf("%f ", (*matrix)[i * dimensionLength + j]);
+                printf("%f ", (*matrix)[i][j]);
             else
-                printf("%f, ", (*matrix)[i * dimensionLength + j]);
+                printf("%f, ", (*matrix)[i][j]);
         }
 
         if (i == dimensionLength - 1)
@@ -82,10 +110,12 @@ void matrixPrint(float** matrix, int dimensionLength){
 }
 
 __host__
-void matrixInitRandomValues(float** matrix, int dimensionLength, float maxValue){
-    for (int i = 0; i < dimensionLength * dimensionLength; i++){
-        // https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
-        (*matrix)[i] = ((float)rand()/(float)(RAND_MAX)) * maxValue;
+void matrixInitRandomValues(float*** matrix, int dimensionLength, float maxValue){
+    for (int i = 0; i < dimensionLength; i++){
+        for (int j = 0; j < dimensionLength; j++){
+            // https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c
+            (*matrix)[i][j] = ((float)rand()/(float)(RAND_MAX)) * maxValue;
+        }
     }
 }
 
@@ -93,26 +123,37 @@ void matrixInitRandomValues(float** matrix, int dimensionLength, float maxValue)
 int main()
 {
     int dimensionLength = 3;
-    size_t arrayByteSize = dimensionLength*dimensionLength*sizeof(float);
-    float *matrix1;
-    float *matrix2;
-    float *matrixOutput;
+    size_t arrayByteSizeP = dimensionLength*sizeof(float*);
+    size_t arrayByteSizeF = dimensionLength*sizeof(float);
+    float **matrix1;
+    float **matrix2;
+    float **matrixOutput;
 
-    matrix1 = (float*) malloc(arrayByteSize);
-    matrix2 = (float*) malloc(arrayByteSize);
-    matrixOutput = (float*) malloc(arrayByteSize);
+    matrix1 = (float**) malloc(arrayByteSizeP);
+    matrix2 = (float**) malloc(arrayByteSizeP);
+    matrixOutput = (float**) malloc(arrayByteSizeP);
+    for (int i = 0; i < dimensionLength; i++){
+        matrix1[i] = (float*) malloc(arrayByteSizeF);
+        matrix2[i] = (float*) malloc(arrayByteSizeF);
+        matrixOutput[i] = (float*) malloc(arrayByteSizeF);
+    }
 
     matrixInitRandomValues(&matrix1, dimensionLength, 100);
     matrixInitRandomValues(&matrix2, dimensionLength, 100);
 
     matrixPrint(&matrix1, dimensionLength);
+    matrixPrint(&matrix2, dimensionLength);
 
     matrixAdd(&matrixOutput, &matrix1, &matrix2, dimensionLength);
 
-    // Verify results
-    matrixPrint(&matrix1, dimensionLength);
+    matrixPrint(&matrixOutput, dimensionLength);
 
     // Free CPU memory
+    for (int i = 0; i < dimensionLength; i++){
+        free(matrix1[i]);
+        free(matrix2[i]);
+        free(matrixOutput[i]);
+    }
     free(matrix1);
     free(matrix2);
     free(matrixOutput);
